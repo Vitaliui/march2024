41359bafa56068b69ab71d579df029316dc20a4e
Vitaliui <donets.vetal@gmail.com>
2024-05-02 03:13:21 +0300

    lesson 1

---
+++ lessons/lesson1/scripts/main.js
@@ -0,0 +1,84 @@
+// var a = 'dasdasd';
+// let b = 1231;
+// const c = false;
+// // d = 'qwqdasda'; // error
+//
+// console.log(c);
+// console.log(b);
+// console.log(c,b,a);
+
+// let x = false;
+// console.log(typeof x);
+
+// let w1 = "okten";
+// let w2 = 'hello';
+//
+// let space = ' ';
+// let result = w1 + space + w2;
+// console.log(result);
+// console.log(100 + 200 + 'ssads');
+// let w3 = '!!!';
+// // result = w1 + space + w2 + space + w3;
+// result = `${w1} ${w2} ${w3}`;
+// console.log(result);
+
+// NUMBERS
+
+// let a = 100;
+// let b = 200;
+// console.log((a + b - 50)/2);
+// // + - * /
+// console.log(11 % 2); // 1
+// console.log(0.1 + 0.2);
+//
+// console.log(BigInt(2121312421412562315673436));
+
+// let b = true;
+//
+// console.log(b);
+// console.log(typeof b);
+// // > < >= == != === !==
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(5 >= 6);
+// console.log(6 <= 6);
+// console.log(6 == '6'); // true -> '6' == '6'
+// console.log(6 === 6);
+// console.log(6 !== 5); // true
+//
+// console.log(!!'asdad');
+// console.log(!!-100500);
+// console.log(!!0); // false
+// console.log(!!''); // false
+// console.log(!!false); // false
+// console.log(!!undefined); // false
+// console.log(!!null); // false
+// console.log(typeof null);
+// console.log(!!NaN); // false
+// console.log(typeof NaN);
+
+// console.log(100 + ''); // string
+// console.log(+'100');
+// console.log(!!100);
+// console.log(!!'asad');
+// console.log(!!!'asad');
+// console.log(!'asd');
+
+let arr = [11, 22, 33, 44]; // object
+let user = {
+    ipn: 1312312,
+    name: 'sergii',
+    surname: 'zhuravlev',
+    bday: '12-12-1999',
+    phone: '+38004124842'
+}
+
+console.log(arr);
+console.log(user);
+
+let users = [
+    {id:1,name: 'sasdas', status: true},
+    {id:2,name: 'kokos', status: false},
+    {id:3,name: 'abrikos', status: true}
+
+]
